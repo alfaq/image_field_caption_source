e4f4cc480eec4054e410faad0915351b976afa2a
alfaq <mozh.by@gmail.com>
2015-09-04 22:04:21 +0300

    source OK

---
+++ image_field_caption.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * $image - contains the image html rendered by Drupal
+ * $caption - contains the image field caption string
+ */
+?>
+<?php print $image; ?>
+<div class="image-field-caption">
+  <?php print $caption; ?>
+  <?php print $source; ?>
+</div>
